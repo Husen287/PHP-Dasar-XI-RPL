191274d5a0ef155d8a57e5be5d0511a41f01ac89
Husen287 <senpasundan@gmail.com>
2025-09-15 13:32:14 +0700

    get & post

---
+++ get & post/latihan1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<titl e>variabel</title>
+</head>
+<body>
+	<?php 
+        $nama="kucen";
+
+        echo   $nama="kucen";
+	 ?>
+
+</body>
+</html>
---
+++ get & post/latihan2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>latihan 2 variabel</title>
+</head>
+<body>
+     <?php 
+         $nama="kucen";
+         $kelas="11 RPL";
+         $alamat="cijambe";
+
+         echo "nama : $nama";
+         echo "<br>";
+         echo "kelas : $kelas";
+         echo "<br>";
+         echo "alamat : $alamat";
+
+      ?>
+</body>
+</html>
