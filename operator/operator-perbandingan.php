92b3d01847242b1857541918edc0d0d9e59bd9d7
Husen287 <senpasundan@gmail.com>
2025-09-15 13:29:43 +0700

    operator

---
+++ operator/operator-perbandingan.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>operator perbandingan</title>
+</head>
+<body>
+	<?php 
+    $n = 10;
+    $i = 20;
+
+	echo "== OPERATOR PEBANDINGAN ==";
+
+       echo "<br>";
+       echo "nilai n adalah $n dan nilai i adalah $i";
+       echo "<br><br>";
+       echo "n == i : ";
+       var_dump($n == $i);
+       echo "<br>";
+       echo "n > i : ";
+       var_dump($n > $i);
+       echo "<br>";
+       echo "n < i : ";
+       var_dump($n < $i);
+       echo "<br>";
+       echo "n >= i : ";
+       var_dump($n >= $i);
+       echo "<br>";
+       echo "n <= i : ";
+       var_dump($n <= $i);
+       echo "<br>";
+       echo "n != i : ";
+       var_dump($n != $i);
+       echo "<br>";
+
+
+
+	 ?>
+
+</body>
+</html>
